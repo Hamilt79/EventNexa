addfbf0cbb1ee30fad483671c173ce6a73e137a9
Austin Hamilton <hamilt79@miamioh.edu>
2023-09-26 13:17:39 +0000

    First Commit

---
+++ killbackground.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+echo "Killing Node"
+pkill node
+echo "Finished Killing Node"
+echo "Killing Npm"
+pkill npm
+echo "Finished Killing Npm"
+echo "Restoring Text"
+stty sane
+echo "Finished Restoring Text"
---
+++ setiptables.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+echo "Configuring ip tables to redirect ports"
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 3000
+echo "Redirect has been configured"
