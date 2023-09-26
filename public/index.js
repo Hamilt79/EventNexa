0ebcd1e16df0fde09ef030f241371218e2b953a3
KuroBlanck <hamilt79@miamioh.edu>
2023-09-26 13:33:30 -0400

    Change button

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 function sendReq(button) {
 	
-	button.value = "Sent";
-	fetch('https://www.pentacle.store/test').then(response => response.text()).then(data => console.log(data));
-	
+	//fetch('https://www.pentacle.store/test').then(response => response.text()).then(data => console.log(data));
+	fetch('https://www.pentacle.store/test').then(function(response) { return response.text(); }).then(function(data) { button.value = data; });
+
 }
