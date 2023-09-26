beb49d99d5efa8c916eaa06b0cd58e2b8c540c48
hamilt79 <hamilt79@miamioh.edu>
2023-09-26 10:54:47 -0400

    aaaa

--- routes/testresponse.js
+++ routes/testresponse.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('You got this lol');
 });
+for(let i = 0; i < 10; ++i) {
+	let ind = i;
+	router.get('/' + ind, function(req, res, next) {  res.send('You got this ' + ind); } );	
+	
+}
 
 module.exports = router;
