64ec72b86a2f1f45ad28b16dcc46e1c5a2ca84fb
KuroBlanck <hamilt79@miamioh.edu>
2023-09-29 04:01:02 -0400

    Added image test

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testRouter = require('./routes/testresponse');
+var imagetestRouter = require('./routes/imagetest');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/test', testRouter);
+app.use('/imagetest', imagetestRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
