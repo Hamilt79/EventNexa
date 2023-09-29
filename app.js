a816d2d7065fd1abb69fef4e640222b26df16578
KuroBlanck <hamilt79@miamioh.edu>
2023-09-29 07:38:28 -0400

    a

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testRouter = require('./routes/testresponse');
-var imagetestRouter = require('./routes/imagetest');
 
 var app = express();
 
@@ -28,7 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/test', testRouter);
-app.use('/imagetest', imagetestRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
