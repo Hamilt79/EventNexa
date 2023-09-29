e2353c5a2b6d90ce299eda02c2116671bc64fc90
Austin Hamilton <hamilt79@miamioh.edu>
2023-09-29 08:23:27 +0000

    a

--- routes/imagetest.js
+++ routes/imagetest.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(__dirname + '/public/imagetest/imagetest.html');
+  res.sendFile(path.join(__dirname + '/../public/imagetest/imagetest.html'));
 });
 
 
