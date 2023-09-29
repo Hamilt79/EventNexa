99fe7cc46a42b29a8b2135306327559383804360
KuroBlanck <hamilt79@miamioh.edu>
2023-09-29 04:16:28 -0400

    a

--- routes/imagetest.js
+++ routes/imagetest.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(__dirname + '/public/testimage/testimage.html');
+  res.sendFile(__dirname + '/public/imagetest/imagetest.html');
 });
 
 
