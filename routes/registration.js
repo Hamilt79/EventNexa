03c0c440645334f8be341226ff458186432aedb1
hamilt79 <hamilt79@miamioh.edu>
2023-10-24 10:47:43 -0400

    Added code for registration test

--- routes/registration.js
+++ routes/registration.js
@@ -2,13 +2,18 @@ const { Console } = require('console');
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-console.error("Error");
+
 /* GET home page. */
+router.post('/', function(req, res) {
+	console.log(JSON.stringify(req.headers));
+});
+
+/*
 router.get('/', function(req, res, next) {
 	console.log("Got request");
 	console.log(JSON.stringify(req.headers));
 	res.send("Got it");
   	//res.sendFile(path.join(__dirname, '/../public/registration/registration.html'));
 });
-
+*/
 module.exports = router;
