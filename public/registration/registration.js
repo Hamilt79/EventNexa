032db04efaef2cc19ed901f30422d139c12d1195
hamilt79 <hamilt79@miamioh.edu>
2023-10-24 10:55:17 -0400

    Added code for registration

--- public/registration/registration.js
+++ public/registration/registration.js
@@ -2,8 +2,14 @@ function sendReq(button) {
 	fetch('https://www.eventnexa.tech/test').then(function(response) { return response.text(); }).then(function(data) { button.value = data; });
 }
 
-function registerRequest(button) {
-	fetch('https://www.eventnexa.tech/registration/createaccount', { method: 'POST', headers: { 'email': 'thisisemail@gmail.com', 'username': 'CoolUsername', 'password': 'VerySecurePassword' } }).then(function(response) { return response.text(); }).then(function(data) { button.value = data; });
+function registerRequest() {
+	const username = document.getElementById('username').value;
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+	const passwordVerify = document.getElementById('passwordverify').value;
+	
+
+	fetch('https://www.eventnexa.tech/registration/createaccount', { method: 'POST', headers: { 'email': email, 'username': username, 'password': password } }).then(function(response) { return response.text(); }).then(function(data) { button.value = data; });
 }
 
 function goProfile() {
