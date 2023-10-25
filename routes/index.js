4204eb6840666dc7f47cd927155fcdaa8fff3ef9
hamilt79 <hamilt79@miamioh.edu>
2023-10-25 12:15:25 -0400

    aaaa

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
 
-/* GET home page. */
+/**
+ * Displays the index.html page when the base website is used
+ * 
+ */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TestLol' });
+  res.render('index', { title: '' });
 });
 
+/**
+ * Redirects the user the the base of
+ * the website when they try to access
+ * the /home page of the website
+ * 
+ */
 router.get('/home', function(req, res, next) {
 	res.redirect('/');
 });
--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,30 @@
+const cyrpto = require('crypto')
 var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-/* GET home page. */
+/**
+ * Gets posts requests directed at /login/loginrequest
+ * The posts requests directed here should include the
+ * user's username and password for verification.
+ * The server should return a token that the user will store in 
+ * their cookies as an authenticator.
+ * When verifying the password it should be sha-512 encoded and
+ * checked against the sha-512 encoded password associated with the
+ * username
+ * 
+ */
 router.post('/', function(req, res) {
 	try{
 		const jsonReq = JSON.stringify(req.headers);
 		const username = jsonReq['username'];
 		const password = jsonReq['password'];
-        
+        // Creates a hash object
+		const hash = crypto.createHash('sha512');
+		// Updates the hash object with the text to turn into sha512 hash
+		hash.update(password);
+		// Getting the hash from the object as a string
+		const passwordHash = hash.digest('hex');
 		res.send("Pass");
 	} catch(ex) {
 		console.log(ex);
