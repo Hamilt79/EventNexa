c6647209f48d0511cf3cb32b5c9d476e6463ba57
hamilt79 <hamilt79@miamioh.edu>
2023-11-08 14:55:53 -0500

    Updated mongodb.js

--- routes/mongodb.js
+++ routes/mongodb.js
@@ -1,21 +1,28 @@
 const { MongoClient } = require("mongodb");
-// Replace the uri string with your connection string.
-const uri = "mongodb://localhost:27017/";
-//const uri = "<connection string uri>";
-const client = new MongoClient(uri);
+// Link to local eventnexa database
+const uri = "mongodb://localhost:27017/eventnexa";
+
+const COLLECTION_E = { 
+    Users: "users"
+ };
 
 async function run() {
+  const client = new MongoClient(uri);
   try {
+    // Create MongoClient to eventnexa dbs
+    // Make variable to the database
+    const database = client.db();
+    //await database.createCollection('users');
+    const users = database.collection(COLLECTION_E.Users);
 
-    const database = client.db('eventnexa');
-    const users = database.collection('users');
+    const userTest = { username: 'John', password: 'Passwordddd!' };
 
-    let myUser = { username : 'John', password: 'Password!' };
+    
 
-    users.insertOne(myUser).then(function() {});
-
-    const movie = await movies.findOne(query);
-    console.log(movie);
+    let query = { username: 'John' };
+    await users.findOne(query).then(value => { console.log(value); });
+    
+    //console.log(movie);
     //console.log("Got here");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -24,3 +31,9 @@ async function run() {
 }
 
 run().catch(console.dir);
+
+module.exports.run = run;
+
+module.exports = {
+  COLLECTION_E
+};
