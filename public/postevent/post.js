fc358bce9efa49a5f6a1b635e8c912474a805bc5
Mexease <Anthony.vlatko@gmail.com>
2023-11-15 16:24:16 -0500

    Testing
    Test

---
+++ public/postevent/post.js
@@ -0,0 +1,13 @@
+function postEvent() {
+    const eventName = document.getElementById('eventName').value;
+    const eventType = document.getElementById('eventType').value;
+    const eventLocation = document.getElementById('eventLocation').value;
+    const eventTime = document.getElementById('eventTime').value;
+
+    console.log('Event Name:', eventName);
+    console.log('Event Type:', eventType);
+    console.log('Event Location:', eventLocation);
+    console.log('Event Time:', eventTime);
+
+
+}
