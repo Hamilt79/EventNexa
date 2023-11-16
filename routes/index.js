f36e41941579e6dcb2ffeaedc31fbeca2fee90a9
Hamilt79 <hamilt79@miamioh.edu>
2023-11-16 12:27:52 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '' });
 });
 
-/**
- * Redirects the user the the base of
- * the website when they try to access
- * the /home page of the website
- * 
- */
-router.get('/home', function(req, res, next) {
-	res.redirect('/');
-});
-
 module.exports = router;
