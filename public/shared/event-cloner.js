2134b37e375b9ff8348adaa11a7f8ffcdab00d1d
hamilt79 <hamilt79@miamioh.edu>
2023-11-16 10:55:50 -0500

    Added some local js

--- public/shared/event-cloner.js
+++ public/shared/event-cloner.js
@@ -1,10 +1,25 @@
 class EventCloner {
+
+    
     static makeEvent() {
         const eventToClone = document.getElementById('event-clone');
         let event = eventToClone.cloneNode(true);
         eventToClone.parentElement.appendChild(event);
-       
         event.style = "";
+        const title = event.querySelector('#event-title')
+        const description = event.querySelector('#event-description')
+        const author = event.querySelector('#event-author')
+        const eventCap = event.querySelector('#event-cap')
+        const eventTime = event.querySelector('#event-time')
+        const address = event.querySelector('#event-address')
+
+        console.log(title);
+        console.log(description);
+        console.log(author);
+        console.log(eventCap);
+        console.log(eventTime);
+        console.log(address);
+
     }
 }
 
---
+++ public/shared/nexa-event.js
@@ -0,0 +1,20 @@
+class NexaEvent {
+    /**
+     * Constructor for NexaEvent.
+     * 
+     * @param {String} title title of event
+     * @param {String} description description of event
+     * @param {String} publisher username of event creator
+     * @param {EventCap} eventCap event cap
+     * @param {Date} eventDate date event starts
+     * @param {*} id eventId
+     */
+    constructor(title, description, publisher, eventCap, eventDate, id) {
+        this.title = title;
+        this.description = description;
+        this.publisher = publisher;
+        this.eventCap = eventCap;
+        this.eventDate = eventDate;
+        this.id = id;
+    }
+}
