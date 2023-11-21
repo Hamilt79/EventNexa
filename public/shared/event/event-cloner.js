fc9e4fa8806a86fd0a16a322e03f1881011e8a76
Austin Hamilton <austin@DESKTOP-UG6OQ6F>
2023-11-21 16:52:45 -0500

    Fixing code for time display

--- public/shared/event/event-cloner.js
+++ public/shared/event/event-cloner.js
@@ -17,7 +17,7 @@ class EventCloner {
         description.value = newEvent.description;
         author.textContent = newEvent.author;
         eventCap.textContent = newEvent.eventCap.joined + "/" + newEvent.eventCap.max;
-        eventTime.textContent = new Date(newEvent.eventDate).toLocaleDateString("YYYY-MM-DDTHH:mm.sssZ");
+        eventTime.textContent = new Date(newEvent.eventDate).toLocaleDateString("YYYY-MM-DDTHH:mm.sssZ", { hour12: true })
         address.textContent = newEvent.address.streetAddress + " " + newEvent.address.city  + " " + newEvent.address.state + " " + newEvent.address.zip;
     }
 }
