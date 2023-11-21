b35ebfdbad8ddaff0dfc342c938db71c8c0b06cf
Austin Hamilton <austin@DESKTOP-UG6OQ6F>
2023-11-21 16:50:47 -0500

    Added code to load events on main page

--- public/shared/event/event-cloner.js
+++ public/shared/event/event-cloner.js
@@ -17,7 +17,7 @@ class EventCloner {
         description.value = newEvent.description;
         author.textContent = newEvent.author;
         eventCap.textContent = newEvent.eventCap.joined + "/" + newEvent.eventCap.max;
-        eventTime.textContent = new Date(newEvent.eventDate);
+        eventTime.textContent = new Date(newEvent.eventDate).toLocaleDateString("YYYY-MM-DDTHH:mm.sssZ");
         address.textContent = newEvent.address.streetAddress + " " + newEvent.address.city  + " " + newEvent.address.state + " " + newEvent.address.zip;
     }
 }
